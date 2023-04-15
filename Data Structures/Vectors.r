91ec5ba9ff5ced80817204b2e7622ef842f623f0
SRAKIB17 <rakibulssc5@gmail.com>
2023-04-15 17:18:54 +0600

    done

---
+++ Data Structures/Vectors.r
@@ -0,0 +1,13 @@
+fruits <- c(TRUE, 10L, 1i, "apple", "orange")
+fruits
+
+# Vector with numerical decimals in a sequence
+numbers1 <- 1.5:6.5 # [1] 1.5 2.5 3.5 4.5 5.5 6.5
+numbers1
+
+# Vector with numerical decimals in a sequence where the last element is not used
+numbers2 <- 1.5:6.3 # [1] 1.5 2.5 3.5 4.5 5.5 6.5
+numbers2
+# !ভেক্টর দৈর্ঘ্য
+fruits <- c("banana", "apple", "orange")
+length(fruits)
